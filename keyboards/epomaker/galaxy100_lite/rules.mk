d2a272207382ca7600065297dcc04f86c381f1a1
daydreamer1982 <vanya.daydreamer@gmail.com>
2026-09-16 02:18:30 +0300

    Update rules.mk

--- keyboards/epomaker/galaxy100_lite/rules.mk
+++ keyboards/epomaker/galaxy100_lite/rules.mk
@@ -56,3 +56,5 @@ SRC += quantum/dynamic_keymap.c
 # Include FS026 SPI driver
 VPATH += lib/chibios-contrib/os/common/ext/CMSIS/ES32/FS026/md
 SRC += lib/chibios-contrib/os/common/ext/CMSIS/ES32/FS026/md/md_spi.c
+
+OPENRGB_ENABLE = yes
